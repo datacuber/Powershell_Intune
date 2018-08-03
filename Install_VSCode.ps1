a0c8a356b7df2a8ba2a88d646b5b058208201b76
Jake Dennis <jd@datacuber.com>
2018-08-03 01:15:24 -0400

    Update Install_VSCode.ps1

--- Install_VSCode.ps1
+++ Install_VSCode.ps1
@@ -1,2 +1,7 @@
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?Linkid=852157" -OutFile "C:\Temp\VSCodeSetup-x64-1.25.1.exe"
 $installerArguments = "/silent /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
-Start-Process  -FilePath 'C:\Users\datacuber\Downloads\VSCodeSetup-x64-1.25.1.exe' -ArgumentList $installerArguments -Wait
+Start-Process  -FilePath 'C:\Users\%Username%\Downloads\VSCodeSetup-x64-1.25.1.exe' -ArgumentList $installerArguments -Wait
